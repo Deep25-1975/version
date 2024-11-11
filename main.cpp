10dd1a61de4e82d829233819ffe44b22f8303d5e
Deep25-1975 <142021151+Deep25-1975@users.noreply.github.com>
2024-11-11 19:23:49 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,5 +3,6 @@
 int main() {
     std::cout << "Hello, World!" << std::endl;
     std::cout << "Hello, Universe!" << std::endl;
+     std::cout << "git version control!" << std::endl;
     return 0;
 }
