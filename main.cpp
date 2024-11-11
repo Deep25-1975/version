98ca1369ec7d6a223e288a82a3da2f12756963e9
Deep25-1975 <142021151+Deep25-1975@users.noreply.github.com>
2024-11-11 19:28:43 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
+# include <math.h>
 
 int main() {
+    int a=5;
+    int b=2;
+    int sum=a+b;
     std::cout << "Hello, World!" << std::endl;
     std::cout << "Hello, Universe!" << std::endl;
      std::cout << "git version control!" << std::endl;
+    std::cout << sum << std ::endl;
     return 0;
 }
